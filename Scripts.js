326d300430439e65dc1f799663d8d56e79448b33
GXavierdev <guilherme.savio.leandro@gmail.com>
2023-06-21 23:10:04 -0300

    primeira pagina javaScript

---
+++ Scripts.js
@@ -0,0 +1,14 @@
+function MatricularCursoHtml(){
+
+    var MatriculasHtml;
+
+
+    LimiteMatriculasHtml = 10;
+
+    MatriculasHtml = MatriculasHtml + 1;
+
+    
+    alert("matricula Efetuada. Qtd Matrículas" & MatricularCursoHtml);
+
+
+}
